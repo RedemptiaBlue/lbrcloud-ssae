53cd9ab74a01b1ffd0d255760118c4f996c911fa
Zachary Vandeventer <zachv94@gmail.com>
2022-03-18 17:33:01 -0400

    added simple get to server

--- server/app.js
+++ server/app.js
@@ -20,9 +20,13 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/', (req, res) => {
+  res.send("Homepage")
+})
+
 app.use('/gifts', giftsRouter)
 app.use('/users', usersRouter)
-//app.use('/db', db)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
